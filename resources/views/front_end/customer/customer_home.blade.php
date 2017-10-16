61b57b0bea6b5c753a4768712fb80afdd506e521
Md. Kamrul Islam <kamrul_bd_islam@yahoo.com>
2017-10-16 23:33:54 +0600

    Update customer_home.blade.php
    text center

--- resources/views/front_end/customer/customer_home.blade.php
+++ resources/views/front_end/customer/customer_home.blade.php
@@ -6,9 +6,10 @@
     
 <div class="top-brands">
     <div class="container">
-    <h4 class="success">Your Order is successfully Submitted. You will contact with you shortly</h4>
+    <h4 class="success text-center">Your Order is successfully Submitted. You will contact with you shortly</h4>
         
         <div class="agile_top_brands_grids">
+        
           
             <div class="clearfix"> </div>
         </div>
